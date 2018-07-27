ff9dde5dd1f986ef8833b705157960ea700d50fb
Ross <ross@sov.tech>
2018-07-27 09:28:16 +0200

    Initial Commit

---
+++ resources/views/default.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('header')
+PolyTherm
+@endsection
+
+@section('title')  Dashboard @endsection
+
+@section('content')
+
+  <example-component></example-component>
+
+@endsection
